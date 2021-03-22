58ff8325a5b321db66ed8a58d4122c8bf75433db
Sandro <sandro.torri@ellysse.it>
2021-03-22 21:20:22 +0100

    finale c

--- js/categoria2020.js
+++ js/categoria2020.js
@@ -78,9 +78,9 @@ matchFinaliC[41] = {"white" : {"username":"marpur", "result":"lost"}, "black" : 
 matchFinaliC[42] = {"white" : {"username":"tizianoparriani", "result":"lost"}, "black" : {"username":"linchetto", "result":"win"}, "url":""}
 matchFinaliC[43] = {"white" : {"username":"giampiero111", "result":"lost"}, "black" : {"username":"thejoker2000", "result":"win"}, "url":""}
 matchFinaliC[44] = {"white" : {"username":"kmoreteam", "result":"win"}, "black" : {"username":"tempiese75", "result":"lost"}, "url":""}
-matchFinaliC[21] = {"white" : {"username":"ytoong", "result":""}, "black" : {"username":"linchetto", "result":""}, "url":""}
+matchFinaliC[21] = {"white" : {"username":"ytoong", "result":"win"}, "black" : {"username":"linchetto", "result":"lost"}, "url":""}
 matchFinaliC[22] = {"white" : {"username":"thejoker2000", "result":"lost"}, "black" : {"username":"kmoreteam", "result":"win"}, "url":"https://www.chess.com/game/daily/313006982"}
-matchFinaliC[11] = {"white" : {"username":"", "result":""}, "black" : {"username":"kmoreteam", "result":""}, "url":""}
+matchFinaliC[11] = {"white" : {"username":"ytoong", "result":""}, "black" : {"username":"kmoreteam", "result":""}, "url":""}
 
 
 
